0863ca746cde62f12a8505a908d85daba6720fbd
Matt Gifford <me@mattgifford.co.uk>
2020-03-30 19:06:46 +0300

    Updated

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,28 @@
 
   <b-container>
 
-    <b-jumbotron header="Cyprus Lockdown Forms" lead="#stayInside #stayhome"></b-jumbotron>
+    <b-jumbotron header="COVID-19 in Cyprus">
+
+      <b-row v-if="stats_loaded" class="text-center" style="margin-top: 40px;">
+
+        <b-col col-sm="4">
+          <h1><span class="text-secondary">{{ currentStats.confirmed }}</span></h1>
+          <p>Confirmed</p>
+        </b-col>
+
+        <b-col col-sm="4">
+          <h1><span class="text-danger">{{ currentStats.deaths }}</span></h1>
+          <p>Deaths</p>
+        </b-col>
+
+        <b-col col-sm="4">
+          <h1><span class="text-success">{{ currentStats.recovered }}</span></h1>
+          <p>Recovered</p>
+        </b-col>
+
+      </b-row>
+
+    </b-jumbotron>
     
     <b-row class="text-center" style="margin-top: 40px;">
 
@@ -32,6 +53,36 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      stats_loaded: false,
+      currentStats: {
+        confirmed: 0,
+        deaths   : 0,
+        recovered: 0
+      }
+    }
+  },
+  created: function(){
+    this.fetchTimeSeries();
+  },
+  methods: {
+    fetchTimeSeries: function(){
+      fetch( "https://pomber.github.io/covid19/timeseries.json" )
+      .then( response => response.json() )
+      .then( data => {
+        data[ "Cyprus" ].forEach( ( { confirmed, recovered, deaths } ) =>
+          // this.currentStats.confirmed = confirmed;
+          this.currentStats = {
+            confirmed,
+            deaths,
+            recovered
+          }
+        );
+        this.stats_loaded = true;
+      } );
+    }
+  }
 }
 </script>
